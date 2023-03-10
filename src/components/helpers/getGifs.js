9618f2536a82fa3b1cd27596760ca5673d9836fa
Marcosgrimoldi <marcos.grimoldi@hotmail.com>
2023-03-10 15:49:36 -0300

    actualizando el helper

--- src/components/helpers/getGifs.js
+++ src/components/helpers/getGifs.js
@@ -2,7 +2,7 @@ export const getGifs = async (category) => {
 
     const url = `https:api.giphy.com/v1/gifs/search?api_key=L3kyn0ufVB5T8T9JFgkOhVZIFYKU4Qj4&q=${ category }&limit=10`;
     const resp = await fetch ( url );
-    const { data =[] } = await resp.json();
+    const { data } = await resp.json();
     const gifs = data.map(img => ({
             id:img.id,
             title:img.title,
